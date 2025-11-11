4dea487070699c6f9d663de63bc184f1853910d9
r-eong <ryungkyung28@gmail.com>
2025-11-11 09:43:56 +0900

    2025-11-11 이령경

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+import Home from './pages/OTT_Home/Home'
+import Login from './pages/OTT_Login/Login'
 
 function App() {
   
 
   return (
     <>
-      <h2>dk</h2>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
