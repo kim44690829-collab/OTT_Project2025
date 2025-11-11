49c4e903800fde588f4e77aee4c8e7e30c9225ba
jhoj0423 <singiru335@gmail.com>
2025-11-11 09:48:39 +0900

    2025-11-11 정호준

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import OTT_Data from './api/OTT_Data'
 
 function App() {
-  
+  const [PopularData,ActionData,AniData,DramaData,ComedyData,SFData] = OTT_Data()
+  console.log(PopularData)
 
   return (
     <>
-      <h2>dk</h2>
+      
     </>
   )
 }
